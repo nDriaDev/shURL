11a04b1794af4cb978537fddb597b1bd2c078edc
4ndr3wc0 <andreacosentino.work@gmail.com>
2023-02-09 10:25:38 +0100

    [FIX] SEO

--- be/controllers/feController.js
+++ be/controllers/feController.js
@@ -14,12 +14,12 @@ export default function feController(req, res, next) {
 		} else {
 			const {DEV, PROD, DETA_SH} = CONSTANTS.PATHS.FE_ROOT_TO_SERVE;
 			let pathFE = process.env.NODE_ENV === CONSTANTS.ENVIRONMENT.DEV ? DEV : process.env.NODE_ENV === CONSTANTS.ENVIRONMENT.DETA_SH ? DETA_SH : PROD;
-			const {INITIAL, GENERATE, SIGNIN, SIGNUP} = CONSTANTS.PATHS.FE;
+			const {INITIAL, GENERATE} = CONSTANTS.PATHS.FE;
 
 			HeadersUtils.setRelAndReportToHeaders(res, req.originalUrl, null, req);
 			//set header noindex for all routes that not are mapped in fe router
 			//so crawler not indexing error page
-			["index.html", INITIAL, GENERATE, SIGNIN, SIGNUP].includes(req.originalUrl) ? res.setHeader("X-Robots-Tag", "index, follow") : res.setHeader("X-Robots-Tag", "noindex, nofollow");
+			["index.html", INITIAL, GENERATE].includes(req.originalUrl) ? res.setHeader("X-Robots-Tag", "index, follow") : res.setHeader("X-Robots-Tag", "noindex, nofollow");
 
 			return res.sendFile(path.join(__dirname, pathFE, 'index.html'));
 		}
